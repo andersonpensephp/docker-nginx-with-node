2e9e7bf078febfacb3d6e3b01ebb7ba3fde78145
andersonfront <andersonpensephp@hotmail.com>
2024-07-18 20:24:39 -0300

    fix: password

--- node/app.js
+++ node/app.js
@@ -9,7 +9,7 @@ const app = async () => {
 	const config = {
 		host: process.env.MYSQL_HOST || 'database',
 		user: process.env.MYSQL_USER || 'root',
-		password: process.env.MYSQL_PASSWORD,
+		password: process.env.MYSQL_PASSWORD || 'root',
 		database: process.env.MYSQL_DATABASE || 'nodedb'
 	}
 	
